6676dbbc73318acd1d90b34eb0b1e416a538fc4f
bhokaresneha <bhokaresneha20@gmail.com>
2022-07-11 22:21:36 +0530

    SptingBoot Rest API Project

---
+++ src/test/java/com/example/restapi_springboot/RestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapi_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
